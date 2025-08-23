1480c40621cad8230261c406043f94840f70ade0
Zhicheng <zhicheng19edu@gmail.com>
2025-08-23 18:30:46 -0400

    Centered the NavBar items

--- apps/web/components/NavBar.tsx
+++ apps/web/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <>
-      <nav className="w-2/3 flex flex-row justify-center h-10">
+      <nav className="w-2/3 flex flex-row items-center h-10">
         <Link href={"/budget"} className="flex-1 text-center">
           Budget
         </Link>
